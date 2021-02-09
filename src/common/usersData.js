47fc9eba4d11f7be2658d91321575d08ee97e23e
Volodymyr Basok <volodymyrquo@gmail.com>
2021-02-09 17:26:53 +0200

    added users table

---
+++ src/common/usersData.js
@@ -0,0 +1,172 @@
+export const usersList = [
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+  {
+    id: 1,
+    name: 'Some Name',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, nemo. Omnis sint voluptatibus unde minus, illum magni quae sequi repudiandae excepturi! A accusantium fugiat deserunt beatae libero debitis veritatis reiciendis.',
+    typeOfUsers: 'Owner',
+    tariff: 'Infinity Gold',
+    country: 'USA',
+    status: 'Active',
+  },
+];
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import s from './Users.module.css';
+import { usersList } from '../../common/usersData';
 
 const Users = () => {
   return (
-    <div>
-      <h1>Hello from Users...</h1>
+    <div className={s.main}>
+      <div className={s.header}>
+        <div>Users</div>
+        <div className={s.button}>
+          <button>+ Add new user</button>
+        </div>
+      </div>
+      <table className={s.table}>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Type of user</th>
+          <th>Tariff</th>
+          <th>Country</th>
+          <th>Status</th>
+        </tr>
+        {usersList.map((obj, index) => (
+          <tr key={index}>
+            {Object.keys(obj).map((keys) => (
+              <td key={obj.id}> {obj[keys]}</td>
+            ))}{' '}
+          </tr>
+        ))}
+      </table>
     </div>
   );
 };
